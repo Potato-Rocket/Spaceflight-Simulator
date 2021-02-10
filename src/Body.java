0179c9d42c2c425dc274fce25f18d219ff59029c
Oscar Stomberg <oscar@stomberg.us>
2021-02-10 11:52:15 +0000

    Restructured the gravity updating code, combined and deleted various methods. There is now a static list of every body which is accessed by an updateAll() function to update the gravity forces and movement for each individual body.

--- src/Body.java
+++ src/Body.java
@@ -13,7 +13,7 @@ public class Body {
   /** Vector to store the body's velocity. */
   private Vector3D velocity;
   /** Vector to store the current forces acting on the body. */
-  private ArrayList<Vector3D> forces;
+  private ArrayList<Vector3D> gravityForces;
   /** Vector to store the body's total acceleration. */
   private Vector3D acceleration;
   /** The body's mass. */
@@ -22,8 +22,8 @@ public class Body {
   private String name;
   /** The body's identification number. */
   private int id;
-  /** The number of bodies */
-  private static int count = 0;
+  /** The list of bodies */
+  private static ArrayList<Body> bodies = new ArrayList<>(0);
 
   /**
    * Class constructor with all values specified by user. Assigns specified values to their
@@ -39,8 +39,12 @@ public class Body {
     this.velocity = velocity;
     this.mass = mass;
     this.name = name;
-    id = count;
-    count++;
+    id = bodies.size();
+    bodies.add(this);
+    gravityForces = new ArrayList<>(bodies.size() - 1);
+    for (Body body : bodies) {
+      body.gravityForces.add(new Vector3D());
+    }
   }
 
   /**
@@ -62,48 +66,40 @@ public class Body {
     new Body(1, "Body");
   }
 
-  /** Create a force for every created body. */
-  public void createForces() {
-    forces = new ArrayList<>(count);
-  }
-
-  /**
-   * Gets the body's position as a 3D vector.
-   *
-   * @return Returns the position.
-   */
-  public Vector3D getPosition() {
-    return position;
-  }
-
-  /**
-   * Gets the body's mass as a double.
-   *
-   * @return Returns the mass.
-   */
-  public double getMass() {
-    return mass;
-  }
-
-  /**
-   * Gets the body's identification number as an integer.
-   *
-   * @return Returns the id.
-   */
-  public int getId() {
-    return id;
-  }
-
   /**
    * Calculates the gravitational pull between this body and another body. Uses their relative
    * positions and masses as well as the gravitational constant to calculate this.
    *
    * @param body second body
    */
-  public void setGravityForce(Body body) {
-    double r = this.position.distanceTo(body.getPosition());
-    double f = G * (this.mass * body.getMass() / Math.pow(r, 2));
-    forces.set(body.getId(), new Vector3D(0, 0, -1 * f));
+  private void setGravityForce(Body body) {
+    if (id != body.id) {
+      double r = position.distanceTo(body.position);
+      double f = G * (mass * body.mass / Math.pow(r, 2));
+      gravityForces.set(body.id, new Vector3D(0, 0, -1 * f));
+    } else {
+      gravityForces.set(id, new Vector3D());
+    }
+  }
+
+  private void update() {
+    acceleration = new Vector3D();
+    for (Vector3D f : gravityForces) {
+      acceleration.addVector(f);
+    }
+    velocity.addVector(acceleration);
+    position.addVector(velocity);
+  }
+
+  public static void updateAll() {
+    for (Body body : bodies) {
+      for (Body other : bodies) {
+        body.setGravityForce(other);
+      }
+    }
+    for (Body body : bodies) {
+      body.update();
+    }
   }
 
   /**
@@ -118,4 +114,5 @@ public class Body {
     return "Body(pos=" + position + ", vel=" + velocity + ", mass=" + mass + ", name=" + name +
         ", id=" + id + ")";
   }
+
 }
--- src/Simulation.java
+++ src/Simulation.java
@@ -4,12 +4,12 @@ public class Simulation {
 
   public static void main(String[] args) {
     ArrayList<Body> bodies = new ArrayList<>();
-    bodies.add(new Body(1000000, "Main"));
-    bodies.add(new Body(new Vector3D(0, 0, 10),
-        new Vector3D(0, 0, 0), 1, "Satellite"));
+    new Body(1000000, "Main");
+    new Body(new Vector3D(0, 0, 10), new Vector3D(0, 0, 0), 1,
+        "Satellite");
 
-    for (Body body : bodies) {
-      body.createForces();
+    for (int x = 0; x < 100; x++) {
+      Body.updateAll();
     }
   }
 
